d8bd610b81c79edceb427262a7e69e8b6b3a7412
daevidvo <itsdavidvo@gmail.com>
2023-05-24 17:49:44 -0700

    completed working on webpack.config.js and src-sw.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, GenerateSW} = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,12 +18,44 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+        new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'text editor'
+        }),
+        new GenerateSW(),
+        new WebpackPwaManifest({
+          name: 'Text Editor',
+          short_name: 'textedit',
+          description: 'Edit text in this application',
+          background_color: '#777777',
+          theme_color: '#000000',
+          start_url: './',
+          icons: [
+            {
+              src: path.resolve('./src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512]
+            }
+          ]
+        })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /^\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /^\.m?js$/,
+          exclude: /^node_modules$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            }
+          }
+        }
       ],
     },
   };
